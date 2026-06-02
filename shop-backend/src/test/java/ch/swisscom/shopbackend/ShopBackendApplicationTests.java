b1d90aa10972987704f0c8645b8dcf10892de518
Paulus <paulbot56@gmail.com>
2026-06-02 16:01:28 +0200

    first commit

---
+++ shop-backend/src/test/java/ch/swisscom/shopbackend/ShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.swisscom.shopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
